e468bb39c4f410e0b4be22cf556ffff940fa9c20
Diego Salazar <kirafreaky@gmail.com>
2015-03-17 13:05:22 -0500

    updated main file
    Created properties and methods of calculator

--- 1. SwiftKalk/SwiftKalk/ViewController.swift
+++ 1. SwiftKalk/SwiftKalk/ViewController.swift
@@ -10,16 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var usuarioEstaDigitandoNumero = false
+    
+    @IBAction func añadirDigito(mensajero: UIButton) {
+        
+        let digito = mensajero.currentTitle!
+        
+        if usuarioEstaDigitandoNumero {
+            display.text = display.text! + digito
+        } else {
+            display.text = digito
+            usuarioEstaDigitandoNumero = true
+        }
+        
+    }
+    
+    @IBAction func operar(mensajero: UIButton) {
+        
+        let operacion = mensajero.currentTitle!
+        
+        switch operacion {
+            case "÷" : hacerOperacion { $1 / $0 }
+            case "×" : hacerOperacion { $0 * $1 }
+            case "−" : hacerOperacion { $1 - $0 }
+            case "+" : hacerOperacion { $0 + $1 }
+            case "√" : hacerOperacion { sqrt($0) }
+            default:break
+        }
+    }
+    
+    func hacerOperacion (operacion: (Double,Double) ->Double) {
+        if stackOperandos.count >= 2 {
+            valorDisplay = operacion(stackOperandos.removeLast(), stackOperandos.removeLast())
+            enter()
+        }
+        
+    }
+    
+    func hacerOperacion (operacion: (Double) ->Double) {
+        if stackOperandos.count >= 2 {
+            valorDisplay = operacion(stackOperandos.removeLast())
+            enter()
+        }
+        
+    }
+    
+    var stackOperandos = [Double]()
+
+    @IBAction func enter() {
+        
+        usuarioEstaDigitandoNumero = false
+        stackOperandos.append(valorDisplay)
+        println("Stack = \(stackOperandos)")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var valorDisplay: Double {
+        get {
+            return (display.text! as NSString!).doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+        }
     }
-
+    
 
 }
 
