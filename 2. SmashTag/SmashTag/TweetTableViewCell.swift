2f5b22e9bcbd612459b5173930e3c11e6ac2d23f
Diego Salazar <kirafreaky@gmail.com>
2015-08-27 11:16:55 -0500

    Custom tweet cell

---
+++ 2. SmashTag/SmashTag/TweetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetTableViewCell.swift
+//  SmashTag
+//
+//  Created by Diego Salazar on 8/27/15.
+//  Copyright (c) 2015 Diego Salazar. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
