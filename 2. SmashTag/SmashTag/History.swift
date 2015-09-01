c8239c12f43c8ac2f860bfc8f4145b1ff3d8579a
Diego Salazar <kirafreaky@gmail.com>
2015-08-31 22:26:14 -0500

    Created Recent searches Class

---
+++ 2. SmashTag/SmashTag/History.swift
@@ -0,0 +1,34 @@
+//
+//  History.swift
+//  SmashTag
+//
+//  Created by Diego Salazar on 8/31/15.
+//  Copyright (c) 2015 Diego Salazar. All rights reserved.
+//
+
+import Foundation
+
+class RecentSearches {
+    private struct Const {
+        static let ValuesKey = "RecentSearches.Values"
+        static let NumberOfSearches = 100
+    }
+    
+    func add(search: String) {
+        var currentSearches = values
+        if let index = find(currentSearches, search) {
+            currentSearches.removeAtIndex(index)
+        }
+        currentSearches.insert(search, atIndex: 0)
+        while currentSearches.count > Const.NumberOfSearches {
+            currentSearches.removeLast()
+        }
+        values = currentSearches
+    }
+    
+    private let defaults = NSUserDefaults.standardUserDefaults()
+    var values: [String] {
+        get { return defaults.objectForKey(Const.ValuesKey) as? [String] ?? [] }
+        set { defaults.setObject(newValue, forKey: Const.ValuesKey) }
+    }
+}
