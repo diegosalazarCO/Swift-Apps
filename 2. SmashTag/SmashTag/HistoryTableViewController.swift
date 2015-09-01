0d1fc4bc198959c75c42a244d7c341b628a6aceb
Diego Salazar <kirafreaky@gmail.com>
2015-08-31 22:26:37 -0500

    Created Recent Searches view controller

---
+++ 2. SmashTag/SmashTag/HistoryTableViewController.swift
@@ -0,0 +1,52 @@
+//
+//  HistoryTableViewController.swift
+//  SmashTag
+//
+//  Created by Diego Salazar on 8/31/15.
+//  Copyright (c) 2015 Diego Salazar. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewController: UITableViewController {
+
+    // MARK: - life cycle
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return RecentSearches().values.count
+    }
+    
+    private struct Storyboard {
+        static let CellReuseIdentifier = "History Cell"
+        static let SegueIdentifier = "Show Search"
+    }
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.CellReuseIdentifier, forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = RecentSearches().values[indexPath.row]
+        return cell
+    }
+
+    // MARK: - Navigation
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier {
+            if identifier == Storyboard.SegueIdentifier {
+                if let ttvc = segue.destinationViewController as? TweetTableViewController {
+                    if let cell = sender as? UITableViewCell {
+                        ttvc.searchText = cell.textLabel?.text
+                    }
+                }
+            }
+        }
+    }
+
+}
